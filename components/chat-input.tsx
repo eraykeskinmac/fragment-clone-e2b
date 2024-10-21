9aa77005bc9637947bb293e9059e0c82d6e204ec
eraykeskinmac <eraykeskinmac@gmail.com>
2024-10-22 01:26:32 +0300

    Refactor chat input component and add chat functionality

--- components/chat-input.tsx
+++ components/chat-input.tsx
@@ -1,3 +1,13 @@
+import TextAreaAutoSize from "react-textarea-autosize";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+import { Button } from "./ui/button";
+import { Paperclip } from "lucide-react";
+
 export function ChatInput({
   error,
   retry,
@@ -16,7 +26,7 @@ export function ChatInput({
   isLoading: boolean;
   stop: () => void;
   input: string;
-  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleInputChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
   handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
   files: File[];
   handleFileChange: (files: File[]) => void;
@@ -24,8 +34,55 @@ export function ChatInput({
   isMultiModal: boolean;
 }) {
   return (
-    <form action="">
-      {error !== undefined && <div>An unexpected error has occurred. Please try again later</div>}
+    <form action="" className="mb-2 flex flex-col mt-auto bg-background">
+      {error !== undefined && (
+        <div>An unexpected error has occurred. Please try again later</div>
+      )}
+      <div className="shadow-md rounded-2xl border">
+        <div className="flex items-center">{children}</div>
+        <TextAreaAutoSize
+          autoFocus={true}
+          minRows={1}
+          maxRows={5}
+          required={true}
+          className="text-normal px-3 resize-none ring-0 bg-inherit w-full m-0 outline-none"
+          placeholder="Describe your app imagination..."
+          value={input}
+          onChange={handleInputChange}
+        />
+        <div>
+          <input
+            type="file"
+            id="multimodal"
+            accept="image/*"
+            multiple={true}
+            className="hidden"
+            onChange={() => {}}
+          />
+          <div>
+            <TooltipProvider>
+              <Tooltip delayDuration={0}>
+                <TooltipTrigger asChild>
+                  <Button
+                    disabled={!isMultiModal}
+                    type="button"
+                    variant={"outline"}
+                    size={"icon"}
+                    className="rounded-xl h-10 w-10"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      document.getElementById("multimodal")?.click();
+                    }}
+                  >
+                    <Paperclip className="h-5 w-5" />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>Add attachemnts</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </div>
+        </div>
+      </div>
     </form>
   );
 }

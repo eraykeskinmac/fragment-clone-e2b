1f98dd195cd29b2e9e177b130d90593d9fd6ff5e
eraykeskinmac <eraykeskinmac@gmail.com>
2024-10-30 11:28:15 +0300

    Refactor chat settings UI to add input fields for language model parameters

--- components/chat-settings.tsx
+++ components/chat-settings.tsx
@@ -92,25 +92,117 @@ export function ChatSettings({
         )}
         <div className="flex flex-col gap-1.5 px-2 py-2">
           <span className="text-sm font-medium">Parameters</span>
-        </div>
-        <div className="flex space-x-4 items-center">
-          <span className="text-sm flex-1 text-muted-foreground">
-            Output Tokens
-          </span>
-          <Input
-            type="number"
-            defaultValue={languageModel.maxTokens}
-            min={50}
-            max={10000}
-            step={1}
-            className="h-6  rounded-sm w-[84px] text-xs text-center tabular-nums"
-            placeholder="Auto"
-            onChange={(e) =>
-              onLanguageModelChange({
-                maxTokens: parseInt(e.target.value) || undefined,
-              })
-            }
-          />
+
+          <div className="flex space-x-4 items-center">
+            <span className="text-sm flex-1 text-muted-foreground">
+              Output Tokens
+            </span>
+            <Input
+              type="number"
+              defaultValue={languageModel.maxTokens}
+              min={50}
+              max={10000}
+              step={1}
+              className="h-6  rounded-sm w-[84px] text-xs text-center tabular-nums"
+              placeholder="Auto"
+              onChange={(e) =>
+                onLanguageModelChange({
+                  maxTokens: parseInt(e.target.value) || undefined,
+                })
+              }
+            />
+          </div>
+          <div className="flex space-x-4 items-center">
+            <span className="text-sm flex-1 text-muted-foreground">
+              Tempature
+            </span>
+            <Input
+              type="number"
+              defaultValue={languageModel.temperature}
+              min={0}
+              max={5}
+              step={0.01}
+              className="h-6  rounded-sm w-[84px] text-xs text-center tabular-nums"
+              placeholder="Auto"
+              onChange={(e) =>
+                onLanguageModelChange({
+                  temperature: parseInt(e.target.value) || undefined,
+                })
+              }
+            />
+          </div>
+          <div className="flex space-x-4 items-center">
+            <span className="text-sm flex-1 text-muted-foreground">Top P</span>
+            <Input
+              type="number"
+              defaultValue={languageModel.topK}
+              min={0}
+              max={1}
+              step={0.01}
+              className="h-6  rounded-sm w-[84px] text-xs text-center tabular-nums"
+              placeholder="Auto"
+              onChange={(e) =>
+                onLanguageModelChange({
+                  topP: parseInt(e.target.value) || undefined,
+                })
+              }
+            />
+          </div>
+          <div className="flex space-x-4 items-center">
+            <span className="text-sm flex-1 text-muted-foreground">Top K</span>
+            <Input
+              type="number"
+              defaultValue={languageModel.topK}
+              min={0}
+              max={500}
+              step={1}
+              className="h-6  rounded-sm w-[84px] text-xs text-center tabular-nums"
+              placeholder="Auto"
+              onChange={(e) =>
+                onLanguageModelChange({
+                  topK: parseInt(e.target.value) || undefined,
+                })
+              }
+            />
+          </div>
+          <div className="flex space-x-4 items-center">
+            <span className="text-sm flex-1 text-muted-foreground">
+              Frequence Penalty
+            </span>
+            <Input
+              type="number"
+              defaultValue={languageModel.frequencyPenalty}
+              min={0}
+              max={2}
+              step={0.01}
+              className="h-6  rounded-sm w-[84px] text-xs text-center tabular-nums"
+              placeholder="Auto"
+              onChange={(e) =>
+                onLanguageModelChange({
+                  frequencyPenalty: parseInt(e.target.value) || undefined,
+                })
+              }
+            />
+          </div>
+          <div className="flex space-x-4 items-center">
+            <span className="text-sm flex-1 text-muted-foreground">
+              Presence Penalty
+            </span>
+            <Input
+              type="number"
+              defaultValue={languageModel.presencePenalty}
+              min={0}
+              max={2}
+              step={0.01}
+              className="h-6  rounded-sm w-[84px] text-xs text-center tabular-nums"
+              placeholder="Auto"
+              onChange={(e) =>
+                onLanguageModelChange({
+                  presencePenalty: parseInt(e.target.value) || undefined,
+                })
+              }
+            />
+          </div>
         </div>
       </DropdownMenuContent>
     </DropdownMenu>

b791a94de3a51860426c708ba9b0ff806abe9ed1
eraykeskinmac <eraykeskinmac@gmail.com>
2024-11-27 19:37:52 +0300

    Add chat functionality enhancements with preview handling and social interaction tracking

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { ChatPicker } from "@/components/chat-picker";
 import { Navbar } from "@/components/navbar";
 import { AuthViewType, useAuth } from "@/lib/auth";
 import { supabase } from "@/lib/supabase";
-import { useState } from "react";
+import { use, useState } from "react";
 import { useLocalStorage } from "usehooks-ts";
 import modelsList from "@/lib/models.json";
 import templates from "@/lib/templates";
@@ -14,7 +14,10 @@ import { ChatSettings } from "@/components/chat-settings";
 import { LLMModelConfig } from "@/lib/models";
 import { Message } from "@/lib/messsages";
 import { DeepPartial } from "ai";
-import { CapsuleSchema } from "@/lib/schema";
+import { CapsuleSchema, capsuleSchema as schema } from "@/lib/schema";
+import { experimental_useObject as useObject } from "ai/react";
+import { usePostHog } from "posthog-js/react";
+import { ExecutionResult } from "@/lib/types";
 
 export default function Home() {
   const [isAuthDialogOpen, setAuthDialog] = useState(false);
@@ -31,6 +34,59 @@ export default function Home() {
   const [capsule, setCapsule] = useState<DeepPartial<CapsuleSchema>>();
   const [currentTab, setCurrentTab] = useState<"code" | "capsule">("code");
   const [isPreviewLoading, setIsPreviewLoading] = useState(false);
+  const [result, setResult] = useState<ExecutionResult>();
+
+  const posthog = usePostHog();
+
+  function setCurrentPreview(preview: {
+    capsule: DeepPartial<CapsuleSchema> | undefined;
+    result: ExecutionResult | undefined;
+  }) {
+    setCapsule(preview.capsule);
+    setResult(preview.result);
+  }
+
+  function setMessage(message: Partial<Message>, index?: number) {
+    setMessage((previousMessages) => {
+      const updatedMessages = [...previousMessages];
+      updatedMessages[index ?? previousMessages.length - 1] = {
+        ...previousMessages[index ?? previousMessages.length - 1],
+        ...message,
+      };
+
+      return updatedMessages;
+    });
+  }
+
+  const { object, submit, isLoading, stop, error } = useObject({
+    api: "/api/chat",
+    schema,
+    onFinish: async ({ object: capsule, error }) => {
+      if (!error) {
+        console.log("code", capsule);
+        setIsPreviewLoading(true);
+        posthog.capture("capsule_generated", {
+          template: capsule?.template,
+        });
+
+        const response = await fetch("/api/sandbox", {
+          method: "POST",
+          body: JSON.stringify({
+            capsule,
+            userID: session?.user.id,
+          }),
+        });
+
+        const result = await response.json();
+        posthog.capture("sandbox_created", { url: result.url });
+        setResult(result);
+        setCurrentPreview({ capsule, result });
+        setMessage({ result });
+        setCurrentTab("capsule");
+        setIsPreviewLoading(false);
+      }
+    },
+  });
 
   function logout() {
     supabase ? supabase.auth.signOut() : console.warn("supabase not loaded");
@@ -51,6 +107,26 @@ export default function Home() {
 
   const lastMessage = messages[messages.length - 1];
 
+  function handleUndo() {
+    setMessages((previousMessages) => [...previousMessages.slice(0, -2)]);
+    setCurrentPreview({ capsule: undefined, result: undefined });
+  }
+
+  function handleClearChat() {
+    stop();
+    setChatInput("");
+    setFiles([]);
+    setMessages([]);
+    setCapsule(undefined);
+    setResult(undefined);
+    setCurrentTab("code");
+    setIsPreviewLoading(false);
+  }
+
+  function handleSocialClick(target: "github" | "x" | "discord") {
+    posthog.capture(`${target}_click`);
+  }
+
   return (
     <main className="flex min-h-screen max-h-screen">
       {supabase && (
@@ -67,18 +143,11 @@ export default function Home() {
             session={session}
             showLogin={() => setAuthDialog(true)}
             signOut={logout}
-            onSocialClick={(target: "github" | "x" | "discord") => {
-              console.log(`Clicked ${target}`);
-            }}
-            onClear={() => {}}
-            canClear={false} // Set this based on your clear functionality state
-            onUndo={() => {
-              // Handle undo functionality
-            }}
-            canUndo={() => {
-              // Return whether undo is available
-              return false;
-            }}
+            onSocialClick={handleSocialClick}
+            onClear={handleClearChat}
+            canClear={messages.length > 0}
+            onUndo={handleUndo}
+            canUndo={messages.length > 1 && !isLoading}
           />
           <Chat />
           <ChatInput
